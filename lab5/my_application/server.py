05fccffb4b6d9d4b87b9b5031dfab6a071ccbc3b
Ubuntu <ubuntu@ip-172-30-0-7.eu-west-1.compute.internal>
2015-10-14 13:17:58 +0000

    Server.py

--- lab5/my_application/server.py
+++ lab5/my_application/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from  flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +6,5 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000,debug=True)
+app.run(host="0.0.0.0",port=8080,debug=True)
+
