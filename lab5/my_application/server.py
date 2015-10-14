4b9ad04a3ac7b1abb1b828a7f72282b9d3b37aee
talytacapitan@gmail.com <talytacapitan@gmail.com>
2015-10-14 15:31:49 +0000

    Server.py new version

--- lab5/my_application/server.py
+++ lab5/my_application/server.py
@@ -1,9 +1,22 @@
 from  flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
+def index():
+    return 'Index File'
+
+@app.route('/hello')
 def hello():
-    return "Hello World!"
+    return 'Hello World'
+
+@app.route('/user/talyta')
+def user():
+    return 'user talyta'
+
+@app.route('/post/80')
+def post():
+    return 'Post 80'
 
 if __name__ == "__main__":
-app.run(host="0.0.0.0",port=8080,debug=True)
+	app.run(host="0.0.0.0",debug=True)
+
