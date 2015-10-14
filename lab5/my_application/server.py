b3ff5402eb40fa5027dd827f43c27260c1258207
talytadias <talytacapitan@gmail.com>
2015-10-14 14:22:06 +0100

    Update server.py

--- lab5/my_application/server.py
+++ lab5/my_application/server.py
@@ -6,9 +6,4 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-<<<<<<< HEAD
 app.run(host="0.0.0.0",port=8080,debug=True)
-
-=======
-    app.run(host="0.0.0.0",port=8080,debug=True)
->>>>>>> fa440460d39b00e8403f2ae76d5112ed569b6cd1
